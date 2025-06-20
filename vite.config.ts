e7729f73b01fb706ddac5d913046c07002877713
ZulfaqarHafez <zulfaqarhafez@gmail.com>
2025-06-20 21:18:19 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/BirthdayOJY/', // <--- ADD OR MODIFY THIS LINE
+});
