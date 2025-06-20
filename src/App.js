fa909006581bcc1496da3f8f19556ff44894be6a
ZulfaqarHafez <zulfaqarhafez@gmail.com>
2025-06-20 21:45:25 +0800

    PUSH update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import SurprisePage from "./pages/SurprisePage";
-const App = () => (_jsx(Router, { children: _jsx(Layout, { children: _jsx(AnimatePresence, { mode: "wait", children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Home, {}) }), _jsx(Route, { path: "/surprise", element: _jsx(SurprisePage, {}) })] }) }) }) }));
+const App = () => (_jsx(Router, { basename: import.meta.env.BASE_URL, children: _jsx(Layout, { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Home, {}) }), _jsx(Route, { path: "/surprise", element: _jsx(SurprisePage, {}) })] }) }) }));
 export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import SurprisePage from "./pages/SurprisePage";
 
 const App: React.FC = () => (
-  <Router>
+  <Router basename={import.meta.env.BASE_URL}>
     <Layout>
-      {/* AnimatePresence for exit animations between routes */}
-      <AnimatePresence mode="wait">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/surprise" element={<SurprisePage />} />
-        </Routes>
-      </AnimatePresence>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/surprise" element={<SurprisePage />} />
+      </Routes>
     </Layout>
   </Router>
 );
