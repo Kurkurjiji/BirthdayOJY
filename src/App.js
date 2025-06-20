5933bf4604dabeb1d48e4c529dd793eda430631a
ZulfaqarHafez <zulfaqarhafez@gmail.com>
2025-06-20 21:57:26 +0800

    deletion

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import SurprisePage from "./pages/SurprisePage";
-const App = () => (_jsx(Router, { basename: import.meta.env.BASE_URL, children: _jsx(Layout, { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Home, {}) }), _jsx(Route, { path: "/surprise", element: _jsx(SurprisePage, {}) })] }) }) }));
-export default App;
